f0a0b855d167fb214f95661e882fb9d27fe3cfe8
Hassan Akhter <akhterhassan89@gmail.com>
2022-10-30 16:31:25 +0500

    Initial Setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./public/Registration Form/form.html"],
+  content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        "epsilondev-purple": "#5267DF",
+        "epsilondev-red": "#FA5959",
+        "epsilondev-blue": "#242A45",
+        "epsilondev-grey": "#9194A2",
+        "epsilondev-white": "#f7f7f7",
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 13%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
   },
   plugins: [],
 };
