2f19f8bb16ab84d4f961ca6fe9560f8fe9dfcfc0
Hassan <67674134+hassan-31x@users.noreply.github.com>
2022-11-16 22:41:21 +0500

    Make corrections in redirect

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,23 +1,33 @@
-
 const form = document.getElementById("form")
+
+function showPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    
     const id = document.getElementById("id").value
     const pass = document.getElementById("password").value
-
-
+    
+    
     $.post("http://epsilon.move.pk/query.php",
-                {
-                    query: "login",
-                    regId: id,
-                    password: pass
-                }, function(data, status){
-                    console.log(data);
-                    if (data === true) {
-                        window.location.href('https://www.youtube.com')
-                    }
-                }
-            );
+        {
+            query: "login",
+            regId: id,
+            password: pass,
+            stayLoggedIn: false
+        }, function(data, status){
+            data = JSON.parse(data);
+            console.log(data);
+            if (data === true) {
+                window.location.href = 'https://www.youtube.com'
+            }
+        } 
+    );
 })
